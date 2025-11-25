3c722a29911c8b281d742a208dfe936d9b35ae6e
Nida <nidasiddiqui.0404@gmail.com>
2025-11-25 13:09:21 +0530

    Add build script for Vercel

---
+++ skillup_backend/build_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Installing dependencies"
+pip install -r requirements.txt
+
+echo "Running migrations (optional)"
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Collecting static files"
+python manage.py collectstatic --noinput
+
+echo "Build complete"
--- skillup_backend/skillup_backend/settings.py
+++ skillup_backend/skillup_backend/settings.py
@@ -155,12 +155,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # optional — where you keep your static source files in dev
-]
+
+# URL to use when referring to static files
+STATIC_URL = '/static/'
+
+# This is where collectstatic will put all the static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
